3a71e41e956386a8b2f33419b22bada75ed38d35
ajSeadler <aj6287@gmail.com>
2024-01-03 12:36:01 -0600

    update

--- threeJS/src/App.jsx
+++ threeJS/src/App.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Sphere from './components/Sphere';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Mars from './components/Mars';
 import JupiterSphere from './components/JupiterSphere';
-import Sun from './components/Sun'
+import Sun from './components/Sun';
 import Mercury from './components/Mercury';
 import VenusSphere from './components/VenusSphere';
 import Saturn from './components/Saturn';
@@ -13,16 +13,17 @@ import Uranus from './components/Uranus';
 import Neptune from './components/Neptune';
 import Pluto from './components/Pluto';
 import UFO from './components/UFO';
+import Error404 from './components/Error404';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-      <Route path="/" element={<UFO />} />
-      <Route path="/sun" element={<Sun />} />
-      <Route path="/mercury" element={<Mercury />} />
-      <Route path="/venus" element={<VenusSphere />} />
+        <Route path="/" element={<UFO />} />
+        <Route path="/sun" element={<Sun />} />
+        <Route path="/mercury" element={<Mercury />} />
+        <Route path="/venus" element={<VenusSphere />} />
         <Route path="/earth" element={<Sphere />} />
         <Route path="/mars" element={<Mars />} />
         <Route path="/jupiter" element={<JupiterSphere />} />
@@ -30,6 +31,8 @@ const App = () => {
         <Route path="/uranus" element={<Uranus />} />
         <Route path="/neptune" element={<Neptune />} />
         <Route path="/pluto" element={<Pluto />} />
+        {/* Add the wildcard route for 404 errors */}
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </>
   );
---
+++ threeJS/src/components/Error404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      {/* Add additional content or styling as needed */}
+    </div>
+  );
+};
+
+export default Error404;
